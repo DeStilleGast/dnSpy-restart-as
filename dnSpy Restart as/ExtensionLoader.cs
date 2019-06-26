0c290d35274c560dd84f8a1e4c0f0563a6b9dff0
DeStilleGast <3677706+DeStilleGast@users.noreply.github.com>
2019-06-26 18:27:18 +0200

    Add project files.

---
+++ dnSpy Restart as/ExtensionLoader.cs
@@ -0,0 +1,22 @@
+﻿using dnSpy.Contracts.Extension;
+using System.Collections.Generic;
+
+namespace dnSpy_Restart_as {
+
+    [ExportExtension]
+    public class ExtensionLoader : IExtension {
+        public ExtensionInfo ExtensionInfo => new ExtensionInfo() {
+            ShortDescription = "Ability to restart as other bit version."
+        };
+
+        public IEnumerable<string> MergedResourceDictionaries {
+            get {
+                yield break;
+            }
+        }
+
+        public void OnEvent(ExtensionEvent @event, object obj) {
+
+        }
+    }
+}
---
+++ dnSpy Restart as/MenuItem.cs
@@ -0,0 +1,52 @@
+﻿using dnSpy.Contracts.App;
+using dnSpy.Contracts.Documents.Tabs.DocViewer;
+using dnSpy.Contracts.Menus;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace dnSpy_Restart_as {
+    static class MenuItem {
+        static class MainMenuConstants {
+            public const string APP_MENU_EXTENSION = "D5B02DE2-E1C7-482A-BFFA-4356A44954D5";
+            public const string RESTART_MENU_32 = "0,BC72617A-72E8-4962-91BB-AF497A84A6D1";
+        }
+
+        [ExportMenu(OwnerGuid = MenuConstants.APP_MENU_GUID, Guid = MainMenuConstants.APP_MENU_EXTENSION, Order = MenuConstants.ORDER_APP_MENU_DEBUG + 0.1, Header = "_Restart dnSpy as")]
+        sealed class RestartMenu : IMenu {
+        }
+
+        [ExportMenuItem(OwnerGuid = MainMenuConstants.APP_MENU_EXTENSION, Header = "Restart as 32 bit", Group = MainMenuConstants.RESTART_MENU_32, Order = 0)]
+        sealed class Restart32Command : MenuItemBase {
+
+
+            public override void Execute(IMenuItemContext context) {
+                ((ICommand)ApplicationCommands.Close).Execute(context);
+
+                Process.Start($"{AppDomain.CurrentDomain.BaseDirectory}\\dnSpy-x86.exe");
+            }
+
+            public override bool IsVisible(IMenuItemContext context) {
+                return IntPtr.Size == 8;
+            }
+        }
+
+        [ExportMenuItem(OwnerGuid = MainMenuConstants.APP_MENU_EXTENSION, Header = "Restart as 64 bit", Group = MainMenuConstants.RESTART_MENU_32, Order = 1)]
+        sealed class Restart64Command : MenuItemBase {
+
+            public override void Execute(IMenuItemContext context) {
+                ((ICommand)ApplicationCommands.Close).Execute(context);
+
+                Process.Start($"{AppDomain.CurrentDomain.BaseDirectory}\\dnSpy.exe");
+            }
+
+            public override bool IsVisible(IMenuItemContext context) {
+                return IntPtr.Size == 4;
+            }
+        }
+    }
+}
