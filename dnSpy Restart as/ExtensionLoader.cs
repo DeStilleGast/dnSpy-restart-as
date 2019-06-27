ae803e1e302507f1fe61a4ab0f2bd1cb98fa17a8
DeStilleGast <3677706+DeStilleGast@users.noreply.github.com>
2019-06-27 12:52:39 +0200

    Added Copyright

--- dnSpy Restart as/ExtensionLoader.cs
+++ dnSpy Restart as/ExtensionLoader.cs
@@ -6,7 +6,8 @@ namespace dnSpy_Restart_as {
     [ExportExtension]
     public class ExtensionLoader : IExtension {
         public ExtensionInfo ExtensionInfo => new ExtensionInfo() {
-            ShortDescription = "Ability to restart as other bit version."
+            ShortDescription = "Ability to restart as other bit version.",
+            Copyright = "DeStilleGast"
         };
 
         public IEnumerable<string> MergedResourceDictionaries {
